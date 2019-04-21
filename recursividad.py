117bb03203bfd8cf659d651ddfa4bfa69d135150
Fernando García <fernandogarcia_fg@outlook.com>
2019-04-21 15:27:53 -0300

    Recursividad

---
+++ recursividad.py
@@ -0,0 +1,60 @@
+# la recursividad es una funcion que para repetirse se invoca a si misma, una funcion es recursiva si para definirse se necesita
+# solo a si misma y se llama a si misma, la recursividad tiene que tener una condicion de parada
+
+def retroContador(numero):
+    print("{}\n".format(numero), end = "")
+    if numero > 0:
+        retroContador(numero - 1)
+            
+def retroContador2(numero):
+    if numero > 0:
+        print("{}\n".format(numero), end = "")
+        while numero > 0:
+            (retroContador(numero - 1))
+            return numero
+    else:
+        pass
+
+        
+def sumatorio(numero):
+    if numero < 0:
+        pass
+    elif numero != 0:
+        return numero + sumatorio(numero - 1)
+    else:
+        return 0
+    
+def sumatorio2(numero):
+    while numero > 0:
+        return numero + sumatorio(numero - 1)
+    return numero
+        
+def factorial(numero):
+    contador = 1
+    while contador != numero:
+        contador += 1
+        return numero * factorial(numero - 1)
+    return numero
+
+def factorial(numero):
+    if numero > 1:
+        contador = 1
+        while contador != numero:
+            contador += 1
+            return numero * factorial(numero - 1)
+    elif numero == 0 or numero == 1:
+        return 1
+    return
+
+def factorial2(numero):
+    if numero > 1:
+        return numero * factorial(numero -1)
+    elif numero == 0 or numero == 1:
+        return 1
+    return
+        
+#retroContador(10)
+#retroContador2(10)
+    
+#print(sumatorio(12))
+#print(sumatorio2(12))
